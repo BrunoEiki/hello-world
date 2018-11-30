480262ac71abd5c5aba69852f142dc512cfc17ad
Eiki <brunoeiki173@gmail.com>
2018-11-30 09:14:51 -0200

    Ateracoes -> Commit

---
+++ new.py
@@ -0,0 +1,10 @@
+#### Comandos úteis em Python
+#
+
+
+#### Console
+# dir()  -->  Mostra os comandos possíveis de uma variável
+# help()  --> Ajuda
+
+from random import sample
+print(sample(range(10), 10))
